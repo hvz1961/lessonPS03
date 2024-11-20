6f4e9a284f70226d44aa61a62ea7544a1a24aec0
hvz1961 <vzkhametov@gmail.com>
2024-11-20 23:09:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,17 +5,13 @@ from googletrans import Translator
 #Создадим мини-игру
 #Создаём функцию, которая будет получать информацию
 
-translator = Translator()
-result = translator.translate('dog', 'ru')
-print(result.text)
 def get_english_words():
     url = 'https://randomword.com/'
     try:
         response = requests.get(url)
-       
         soup = BeautifulSoup(response.content, 'html.parser')
         english_words = soup.find('div', id='random_word').text.strip()
-        word_definition = soup.find('div', 'id=rundom_word_definition').text.strip()
+        word_definition = soup.find('div', id='random_word_definition').text.strip()
 
         return {
             'english_words': english_words,
@@ -23,20 +19,28 @@ def get_english_words():
         }
     except:
         print('Произошда ошибка')
-
+        return None
+def translate_word(word):
+    translator = Translator()
+    translation_word = translator.translate(word, src='en', dest='ru')
+    return translation_word.text
 def word_game():
     print('Добро пожаловать в игру')
     while True:
         word_dict = get_english_words()
         word = word_dict.get('english_words')
         word_definition = word_dict.get('word_definition')
-
         print(f'Значение слова - {word_definition}')
+
         user = input('Что это за слово?')
         if user == word:
             print('Все правильно')
+            translation = translate_word(word)
+            print(f'Перевод слова "{word}": {translation}')
         else:
             print(f'Ответ неверный, было слово - {word}')
+            translation = translate_word(word)
+            print(f'Перевод слова "{word}": {translation}')
 
         play_again = input('Хотите сыграть еще раз? y/n')
         if play_again != 'y':
@@ -46,7 +50,9 @@ def word_game():
 word_game()
             
     
-
+# translator = Translator()
+# result = translator.translate('dog', 'ru')
+# print(result.text)
 
         
 
